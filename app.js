7a9c83e9143e69d1ae6b1add0d527e49d901d721
Gabriel Lemos Rodrigues <55399020+Lemos00@users.noreply.github.com>
2020-05-01 16:50:50 -0400

    initial files

---
+++ app.js
@@ -0,0 +1,68 @@
+import Tool from './tool.class.js'; // we might have to use "/" only idk
+
+document.querySelectorAll("[data-command]").forEach( // maybe remove ]
+    item => {
+        item.addEventListener("click", e => {
+            console.log(item.getAttribute("data-command"));
+        })
+    }//e is the element
+); //this way, you will get all the elements that have the data command specified
+
+document.querySelectorAll("[data-tool]").forEach(
+    item => (
+        item.addEventListener("click", e => {
+            document.querySelector("[data-tool].active").classList.toggle("active"); // remove the previous active function from the active class
+            
+            item.classList.add("active"); // we add the element we clicked on to the active class
+             
+            //with the tool.class.js created:
+            let selectedTool = item.getAttribute("data-tool");
+            
+            switch(selectedTool){
+                //activate shape or line widths group
+                case Tool.TOOL_SQUARE:
+                case Tool.TOOL_TRIANGLE:
+                case Tool.TOOL_PAINT_BUCKET:
+                case Tool.TOOL_ERASER:
+                case Tool.TOOL_BRUSH:
+                case Tool.TOOL_PENCIL:
+                    //make pencil shapes visible
+                    document.querySelector(".group.for-shapes").style = "display: block;";
+                    //make brush sizes invisible
+                    console.log()
+                    document.querySelector(".group.for-brush").style = "display: none;";
+                    break;
+
+                case Tool.TOOL_BRUSH:
+                    //make pencil shapes invisible
+                    document.querySelector(".group.for-shapes").style.display = "none";
+                    //make brush selection visible
+                    document.querySelector(".group.for-brush").style.display = "block";
+                    break;
+                default:
+                    //make both line groups invisible
+                    document.querySelector(".group.for-shapes").style.display = "block";
+                    document.querySelector(".group.for-brush").style.display = "block";
+            }
+
+        }
+    ))
+);
+
+document.querySelectorAll("[data-line-width]").forEach(
+    item => {
+        item.addEventListener("click", e => {
+            document.querySelector("[data-line-width].active").classList.toggle("active"); // remove the previous active function from the active class
+            item.classList.add("active"); // we add the element we clicked on to the active class
+        });
+    }
+);
+
+document.querySelectorAll("[data-color]").forEach(
+    item => {
+        item.addEventListener("click", e => {
+            document.querySelector("[data-color].active").classList.toggle("active"); // remove the previous active function from the active class
+            item.classList.add("active"); // we add the element we clicked on to the active class
+        });
+    }
+);
---
+++ tool.class.js
@@ -0,0 +1,9 @@
+export default class Tool{
+
+    static TOOL_SQUARE = "square";
+    static TOOL_TRIANGLE = "triangle";
+    static TOOL_PAINT_BUCKET = "paint-bucket";
+    static TOOL_PENCIL = "pencil";
+    static TOOL_BRUSH = "brush";
+    static TOOL_ERASER = "eraser";
+}
